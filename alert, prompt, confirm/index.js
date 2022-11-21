33e9c24503b61ffc785f7a872f9281f231a148f5
dip <gitworkingfiles@gmail.com>
2022-11-21 23:08:45 +0600

    first commit

---
+++ alert, prompt, confirm/index.js
@@ -0,0 +1,10 @@
+console.log("this is a message");
+
+// let message = alert( " happy death day! "  + " you are going to die soon !:)");
+// console.log(message);
+
+// let dataput = prompt("enter your death time as you want" , ":)");
+// console.log(dataput);
+
+let beconfirm = confirm("DO YOU REALLY WANT TO DELETE THIS POST?");
+console.log(beconfirm);
---
+++ functions/app.js
@@ -0,0 +1,49 @@
+// function emo(num) {
+//     let result = num + num;
+//     console.log("this is the result = " + result);
+// }
+// emo(2);
+
+// let name ="harry";
+// let name1 = "andrew";
+// let name2= "rock";
+
+// function miter(name) {
+//     let pain =  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, asperiores." + name ;
+//     console.log(pain);
+//     // console.log(name + " is a good man!");
+    
+// }
+
+// miter(name);
+
+
+// function back(a,b,c) {
+//     let d = a + b + c;
+//     let king = a - b + c;
+//     return king;
+//     return d;
+// }
+
+// let returnval = back(1,2,3);
+// console.log(returnval);
+
+
+
+
+function greet(name,greetText) {
+    
+    console.log(greetText + " " + name);
+    console.log(name + " " + "is a good boy");
+};
+
+let name = "harry";
+let name1 = "shubam";
+let name2 = "rohan";
+let name3 = "sagar";
+let greetText = "good morning";
+
+greet(name, greetText,);
+greet(name1, greetText);
+greet(name2, greetText);
+greet(name3, greetText);
